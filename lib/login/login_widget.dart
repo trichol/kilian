60dd77a4c0a734a73edb87c5001a5a115cc5b8d8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-01 05:43:36 +0000

    Updating to latest FlutterFlow output.

--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -2,7 +2,6 @@ import '/auth/firebase_auth/auth_util.dart';
 import '/backend/api_requests/api_calls.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -1942,7 +1941,7 @@ class _LoginWidgetState extends State<LoginWidget>
                                           Row(
                                             mainAxisSize: MainAxisSize.max,
                                             mainAxisAlignment:
-                                                MainAxisAlignment.center,
+                                                MainAxisAlignment.spaceEvenly,
                                             crossAxisAlignment:
                                                 CrossAxisAlignment.center,
                                             children: [
@@ -2183,24 +2182,6 @@ class _LoginWidgetState extends State<LoginWidget>
                                                         const Color(0xFFF1F4F8),
                                                   ),
                                                 ),
-                                              ),
-                                              FlutterFlowIconButton(
-                                                borderRadius: 8.0,
-                                                buttonSize: 40.0,
-                                                fillColor:
-                                                    FlutterFlowTheme.of(context)
-                                                        .primary,
-                                                icon: Icon(
-                                                  Icons.apple,
-                                                  color: FlutterFlowTheme.of(
-                                                          context)
-                                                      .info,
-                                                  size: 24.0,
-                                                ),
-                                                onPressed: () {
-                                                  print(
-                                                      'IconButton pressed ...');
-                                                },
                                               ),
                                             ],
                                           ),
